25819fb431f66789de483853d95e170778769798
Miquel Roset <mikelroset@gmail.com>
2024-03-29 12:02:51 +0100

    Remove route middleware

--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -1,12 +1,7 @@
 import express from "express";
 import { getHome, getAbout } from "../controllers/index.controllers.js";
-var router = express.Router();
 
-// middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
-});
+var router = express.Router();
 
 router.get('/', getHome);
 router.get('/about', getAbout);

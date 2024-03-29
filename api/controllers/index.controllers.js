9010d7386bc7830f72d7616d97539a3d6bae19d6
Miquel Roset <mikelroset@gmail.com>
2024-03-29 11:48:24 +0100

    Add controller system

---
+++ api/controllers/index.controllers.js
@@ -0,0 +1,7 @@
+export const getHome = (req, res) => {
+    res.send('Birds home page.');
+};
+
+export const getAbout = (req, res) => {
+    res.send('About birds.');
+};
--- api/routes/index.routes.js
+++ api/routes/index.routes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { getHome, getAbout } from "../controllers/index.controllers.js";
 var router = express.Router();
 
 // middleware that is specific to this router
@@ -6,13 +7,8 @@ router.use(function timeLog(req, res, next) {
   console.log('Time: ', Date.now());
   next();
 });
-// define the home page route
-router.get('/', function(req, res) {
-  res.send('Birds home page');
-});
-// define the about route
-router.get('/about', function(req, res) {
-  res.send('About birds');
-});
+
+router.get('/', getHome);
+router.get('/about', getAbout);
 
 export default router;

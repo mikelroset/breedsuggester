ea4ac52dad36652322a79dc6220dac630a837e0b
Miquel Roset <mikelroset@gmail.com>
2024-03-30 14:27:38 +0100

    Refactor mysql connection

--- database/connection.js
+++ database/connection.js
@@ -1,25 +1,40 @@
-import mysql from 'mysql2/promise';
+import { createPool } from 'mysql2/promise';
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = process.env;
+const WAIT_FOR_CONNECTIONS = true;
+const CONNECTION_LIMIT = 10;
+const QUEUE_LIMIT = 0;
 
-let connection;
+let pool;
 
 try {
-  async function connectDB() {
-    connection = await mysql.createConnection({
-      host: DB_HOST,
-      user: DB_USER,
-      database: DB_NAME,
-      port: DB_PORT,
-      password: DB_PASSWORD,
+    pool = createPool({
+        host: DB_HOST,
+        user: DB_USER,
+        database: DB_NAME,
+        port: DB_PORT,
+        password: DB_PASSWORD,
+        waitForConnections: WAIT_FOR_CONNECTIONS,
+        connectionLimit: CONNECTION_LIMIT,
+        queueLimit: QUEUE_LIMIT,
     });
-  }
 
-  connectDB().catch(err => {
-    console.error('Error al conectar a la base de datos:', err);
-  });
-} catch (err) {
-  console.error('Error al configurar la conexión a la base de datos:', err);
+    console.log('Database connection established successfully.');
+} catch (error) {
+    console.error('Error connecting to the database:', error.message);
 }
 
-export default connection;
+// Error handling when creating the connection pool
+pool.on('error', (err) => {
+    console.error('Error in the connection pool:', err.message);
+});
+
+// Closing the connection pool when closing the application
+process.on('SIGINT', () => {
+    pool.end().then(() => {
+        console.log('Connection pool closed.');
+        process.exit(0);
+    });
+});
+
+export default pool;

95dc9115894dd73d90b07d944abf2c39cdb1a16b
Miquel Roset <miquelroset@Miquels-MacBook-Pro-2.local>
2024-04-04 21:52:57 +0200

    Change traits var name to result

--- api/controllers/traits.controllers.js
+++ api/controllers/traits.controllers.js
@@ -1,37 +1,35 @@
-import { 
-    findTraitsByLanguage, 
-    findTraitById 
+import {
+  findTraitsByLanguage,
+  findTraitById,
 } from "../models/traits.models.js";
 
 export const getTraits = async (req, res) => {
-
-    try {
-        const traits = await findTraitsByLanguage(req.language);
-        res.json(traits[0]);
-    } catch (error) {
-        console.error("Error retrieving traits:", error);
-        res.status(500).send("Internal Server Error");
-    }
+  try {
+    const result = await findTraitsByLanguage(req.language);
+    res.json(result[0]);
+  } catch (error) {
+    console.error("Error retrieving traits:", error);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 export const getTrait = async (req, res) => {
+  const id = parseInt(req.params.id);
 
-    const id = parseInt(req.params.id);
+  if (isNaN(id)) {
+    return res.status(400).send("Invalid trait ID");
+  }
 
-    if (isNaN(id)) {
-        return res.status(400).send("Invalid trait ID");
+  try {
+    const result = await findTraitById(id, req.language);
+
+    if (result[0].length > 0) {
+      res.json(result[0]);
+    } else {
+      res.status(404).send("Trait not found");
     }
-
-    try {
-        const trait = await findTraitById(id, req.language);
-
-        if (trait[0].length > 0) {
-            res.json(trait[0]);
-        } else {
-            res.status(404).send("Trait not found");
-        }
-    } catch (error) {
-        console.error("Error retrieving trait:", error);
-        res.status(500).send("Internal Server Error");
-    }
+  } catch (error) {
+    console.error("Error retrieving trait:", error);
+    res.status(500).send("Internal Server Error");
+  }
 };
